ec592c12b200d5df641596e4abe920aa7f3f066d
Максим Соловьев <maximnoch@gmail.com>
2023-02-22 21:29:48 +0300

    Last real actual version

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def choice_char_class() -> str:
 
 
 if __name__ == '__main__':
-    run_screensaver
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
